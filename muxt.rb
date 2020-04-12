da106dde9ce45cbb519f206f5efa7e7005e4edfd
Dan Weinand <dweinand@gmail.com>
2020-04-12 12:20:34 -0700

    Update muxt to 1.0.3 (#2)

--- muxt.rb
+++ muxt.rb
@@ -2,10 +2,9 @@ class Muxt < Formula
   desc "Command-line tool for setting up and starting tmux environments."
   homepage "https://github.com/dweinand/muxt"
 
-  version "1.0.1"
-  sha256 "0dd78cb8f55d2982b6ad34598fe6ad91abd8a4e69ea89981840aa3e8bc92062e"
-
-  url "https://github.com/dweinand/muxt/releases/download/v1.0.1/muxt_1.0.1_darwin-amd64.tar.gz"
+  url "https://github.com/dweinand/muxt/releases/download/v1.0.3/muxt_v1.0.3_darwin-amd64.tar.gz"
+  version "1.0.3"
+  sha256 "2ea1a171f11773a2e0cd7ba58ca8ea42defdef2d73fbfe4a44143deadd6d423b"
 
   def install
     bin.install "muxt"
